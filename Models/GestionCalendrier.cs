9e5d39dc56f166759410af831880c22b0a69f433
Paul Michaud <awsome_paul@live.ca>
2024-11-07 16:09:16 -0500

    Cération Classe calendrier

---
+++ Models/GestionCalendrier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate.Models
+{
+    public class GestionCalendrier
+    {
+		private List<Journee> _journees;
+
+		public List<Journee> Journees
+		{
+			get { return _journees; }
+			set { _journees = value; }
+		}
+
+		private List<int> _annee;
+
+		public List<int> Annee
+		{
+			get { return _annee; }
+			set { _annee = value; }
+		}
+
+		
+
+	}
+}
